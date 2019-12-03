62b6e314d1254039e891d61d83031316f21b6efa
hdq <haqsir@163.com>
2019-12-03 23:12:11 +0800

    1.初始化

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Button } from 'antd';
 import './App.css';
 
 const App: React.FC = () => {
@@ -10,14 +11,7 @@ const App: React.FC = () => {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button type="primary">Button</Button>
       </header>
     </div>
   );
